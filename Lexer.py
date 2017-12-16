a42e30d405ddc04f2688709e83d3a5127307a087
Sandy Chen <sandy2142@qq.com>
2017-12-16 18:43:48 +0900

    Updated 2017/12/16
    Structures Changed
    Lexer Done

--- Lexer.py
+++ Lexer.py
@@ -1,102 +1,143 @@
-class Lexer:
-    '''Word Analysis'''
+from token import *
 
-    def __init__(self):
+class Lexer(object):
+    '''词法分析器'''
+    content = None
+    tokens = []
+
+    def __init__(self, content):
         self.tokens = []
+        self.content = content
 
+    # 判断是否是空白字符
     def is_blank(self, index):
-        return content[index] == ' ' or content[index] == '\t' or content[index] == '\n' or content[index] == '\r'
+        return self.content[index] == ' ' or self.content[index] == '\t' or self.content[index] == '\n' or self.content[index] == '\r'
 
-    # Skip Blank
+    # 跳过空白字符
     def skip_blank(self, index):
-        while index < len(content) and self.is_blank(index):
-            index = index + 1
+        while index < len(self.content) and self.is_blank(index):
+            index += 1
         return index
 
+    # 打印
     def print_log(self, style, value):
-        print('(%s, %s)' % (style, value))
+        print ('(%s, %s)' % (style, value))
 
+    # 判断是否是关键字
     def is_keyword(self, value):
         for item in keywords:
             if value in item:
                 return True
         return False
 
-    # Main Code of Lexer
+    # 词法分析主程序
     def main(self):
         i = 0
-        while i < len(content):
+        while i < len(self.content):
             i = self.skip_blank(i)
-        if content[i] == '#':
-            self.tokens.append(Token(4, content[i]))
-            i = self.skip_blank(i + 1)
-            # head files
-            while i < len(content):
-                # match "include"
-                if re.match('include', content[i:]):
-                    self.tokens.append(Token(0, 'include'))
-                    i = self.skip_blank(i + 7)
-                # match " or v
-                elif content[i] == '\"' or content[i] == '<':
-                    self.tokens.append(Token(4, content[i]))
-                    i = self.skip_blank(i + 1)
-                    close_string = '\"' if content[i] == '\"' else '>'
-                    # find included lib
-                    lib = ''
-                    while content[i] != close_string:
-                        lib += content[i]
-                        i += 1
-                    self.tokens.append(Token(4, close_string))
-                    i = self.skip_blank(i + 1)
-                    break
-                else:
-                    print ('include error!')
-                    exit()
-
-        #if content begins with letter or down slash
-        elif content[i].isalpha() or content[i] == '_':
-            #find the string
-            temp = ''
-            while i < len(content) and (content[i].isalpha() or content[i]=='_' or content[i].isdigit()):
-                temp += content[i]
-                i += 1
-            #analyze string
-            if self.is_keyword(temp):
-                self.tokens.append(Token(0,temp))
-            else:
-                self.tokens.append(Token(1,temp))
-            i = self.skip_blank(i)
-        #start with digit
-        elif content[i].isdigit():
-            temp = ''
-            while i < len(content):
-                temp += content[i]
-                i += 1
-                #analyze string
-                if content[i].isdigit() or (content[i] == '.' and content[i + 1].isdigit()):
-                    temp += content[i]
-                    i += 1
-                elif not content[i].isdigit():
-                    if content[i] == '.':
-                        print ('float number not supported yet')
+            # 如果是引入头文件，还有一种可能是16进制数，这里先不判断
+            if self.content[i] == '#':
+                #self.print_log( '分隔符', self.content[ i ] )
+                self.tokens.append(Token(4, self.content[i]))
+                i = self.skip_blank(i + 1)
+                # 分析这一引入头文件
+                while i < len(self.content):
+                    # 匹配"include"
+                    if re.match('include', self.content[i:]):
+                        # self.print_log( '关键字', 'include' )
+                        self.tokens.append(Token(0, 'include'))
+                        i = self.skip_blank(i + 7)
+                    # 匹配"或者<
+                    elif self.content[i] == '\"' or self.content[i] == '<':
+                        # self.print_log( '分隔符', self.content[ i ] )
+                        self.tokens.append(Token(4, self.content[i]))
+                        i = self.skip_blank(i + 1)
+                        close_flag = '\"' if self.content[i] == '\"' else '>'
+                        # 找到include的头文件
+                        lib = ''
+                        while self.content[i] != close_flag:
+                            lib += self.content[i]
+                            i += 1
+                        # self.print_log( '标识符', lib )
+                        self.tokens.append(Token(1, lib))
+                        # 跳出循环后，很显然找到close_flog
+                        # self.print_log( '分隔符', close_flag )
+                        self.tokens.append(Token(4, close_flag))
+                        i = self.skip_blank(i + 1)
+                        break
+                    else:
+                        print ('include error!')
                         exit()
-                    else:
-                        break
-            self.tokens.append(Token(2,temp))
-            i = self.skip_blank(i)
-        #Begin with seperator
-        elif content[i] in delimiters:
-            self.tokens.append(Token(4,content[i]))
-            if content[i] == '\"':
-                i += 1
+            # 如果是字母或者是以下划线开头
+            elif self.content[i].isalpha() or self.content[i] == '_':
+                # 找到该字符串
                 temp = ''
-                while i < len(content):
-                    if not contents[i]=='\"':
-                        temp += content[i]
-                        i += 1
-                    else:
-                        break
+                while i < len(self.content) and (self.content[i].isalpha() or self.content[i] == '_' or self.content[i].isdigit()):
+                    temp += self.content[i]
+                    i += 1
+                # 判断该字符串
+                if self.is_keyword(temp):
+                    # self.print_log( '关键字', temp )
+                    self.tokens.append(Token(0, temp))
                 else:
-                    print('error: lack of \"')
-                    exit()
-                self.tokens.append(Token(5,temp))
+                    # self.print_log( '标识符', temp )
+                    self.tokens.append(Token(1, temp))
+                i = self.skip_blank(i)
+            # 如果是数字开头
+            elif self.content[i].isdigit():
+                temp = ''
+                while i < len(self.content):
+                    if self.content[i].isdigit() or (self.content[i] == '.' and self.content[i + 1].isdigit()):
+                        temp += self.content[i]
+                        i += 1
+                    elif not self.content[i].isdigit():
+                        if self.content[i] == '.':
+                            print ('float number error!')
+                            exit()
+                        else:
+                            break
+                # self.print_log( '常量' , temp )
+                self.tokens.append(Token(2, temp))
+                i = self.skip_blank(i)
+            # 如果是分隔符
+            elif self.content[i] in delimiters:
+                # self.print_log( '分隔符', self.content[ i ] )
+                self.tokens.append(Token(4, self.content[i]))
+                # 如果是字符串常量
+                if self.content[i] == '\"':
+                    i += 1
+                    temp = ''
+                    while i < len(self.content):
+                        if self.content[i] != '\"':
+                            temp += self.content[i]
+                            i += 1
+                        else:
+                            break
+                    else:
+                        print ('error:lack of \"')
+                        exit()
+                    # self.print_log( '常量' , temp )
+                    self.tokens.append(Token(5, temp))
+                    # self.print_log( '分隔符' , '\"' )
+                    self.tokens.append(Token(4, '\"'))
+                i = self.skip_blank(i + 1)
+            # 如果是运算符
+            elif self.content[i] in operators:
+                # 如果是++或者--
+                if (self.content[i] == '+' or self.content[i] == '-') and self.content[i + 1] == self.content[i]:
+                    # self.print_log( '运算符', self.content[ i ] * 2 )
+                    self.tokens.append(Token(3, self.content[i] * 2))
+                    i = self.skip_blank(i + 2)
+                # 如果是>=或者<=
+                elif (self.content[i] == '>' or self.content[i] == '<') and self.content[i + 1] == '=':
+                    # self.print_log( '运算符', self.content[ i ] + '=' )
+                    self.tokens.append(Token(3, self.content[i] + '='))
+                    i = self.skip_blank(i + 2)
+                # 其他
+                else:
+                    # self.print_log( '运算符', self.content[ i ] )
+                    self.tokens.append(Token(3, self.content[i]))
+                    i = self.skip_blank(i + 1)
+
+        for token in self.tokens:
+          print ('(%s, %s)' % (token.type, token.value))
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 '''
 A Simple C-Compiler based in Python
 
+For Help:
+python main.py -h
+
+For Lexical Result:
+python main.py -s source.c -p
+
 '''
 
 import re
 import sys
 import getopt
 
-from Token import *
-from Lexer import *
+
 
 # Classifications of Token
 from sqlalchemy import true
@@ -38,10 +43,162 @@ delimiters = ['(', ')', '{', '}', '[', ']', ',', '\"', ';']
 file_name = None
 content = None
 
+class Token:
+    '''Analyze the Tokens'''
+    def __init__(self, type_index, value):
+        self.type = DETAIL_TOKEN_STYLE[
+            value] if type_index == 0 or type_index == 3 or type_index == 4 else TOKEN_STYLE[type_index]
+        self.value = value
+
+
+
+from token import *
+
+class Lexer(object):
+    '''Lexical Analyzer'''
+    tokens = []
+
+    def __init__(self):
+        self.tokens = []
+
+    # Blank String
+    def is_blank(self, index):
+        return content[index] == ' ' or content[index] == '\t' or content[index] == '\n' or content[index] == '\r'
+
+    # Skip Blank String
+    def skip_blank(self, index):
+        while index < len(content) and self.is_blank(index):
+            index += 1
+        return index
+
+    # Print
+    def print_log(self, style, value):
+        print ('(%s, %s)' % (style, value))
+
+    # Judge Keyword
+    def is_keyword(self, value):
+        for item in keywords:
+            if value in item:
+                return True
+        return False
+
+    # Lexer Main
+    def main(self):
+        i = 0
+        while i < len(content):
+            i = self.skip_blank(i)
+            # include file
+            if content[i] == '#':
+                self.tokens.append(Token(4, content[i]))
+                i = self.skip_blank(i + 1)
+                # analyze
+                while i < len(content):
+                    # find "include"
+                    if re.match('include', content[i:]):
+                        self.tokens.append(Token(0, 'include'))
+                        i = self.skip_blank(i + 7)
+                    # find " or <
+                    elif content[i] == '\"' or content[i] == '<':
+                        self.tokens.append(Token(4, content[i]))
+                        i = self.skip_blank(i + 1)
+                        close_flag = '\"' if content[i] == '\"' else '>'
+                        # find include file
+                        lib = ''
+                        while content[i] != close_flag:
+                            lib += content[i]
+                            i += 1
+                        self.tokens.append(Token(1, lib))
+                        self.tokens.append(Token(4, close_flag))
+                        i = self.skip_blank(i + 1)
+                        break
+                    else:
+                        print ('include error!')
+                        exit()
+
+            elif content[i].isalpha() or content[i] == '_':
+                # find string
+                temp = ''
+                while i < len(content) and (content[i].isalpha() or content[i] == '_' or content[i].isdigit()):
+                    temp += content[i]
+                    i += 1
+                # analyze string
+                if self.is_keyword(temp):
+                    self.tokens.append(Token(0, temp))
+                else:
+                    self.tokens.append(Token(1, temp))
+                i = self.skip_blank(i)
+            # if digits
+            elif content[i].isdigit():
+                temp = ''
+                while i < len(content):
+                    if content[i].isdigit() or (content[i] == '.' and content[i + 1].isdigit()):
+                        temp += content[i]
+                        i += 1
+                    elif not content[i].isdigit():
+                        if content[i] == '.':
+                            print ('float number error!')
+                            exit()
+                        else:
+                            break
+                self.tokens.append(Token(2, temp))
+                i = self.skip_blank(i)
+            # delimiters
+            elif content[i] in delimiters:
+                self.tokens.append(Token(4, content[i]))
+                if content[i] == '\"':
+                    i += 1
+                    temp = ''
+                    while i < len(content):
+                        if content[i] != '\"':
+                            temp += content[i]
+                            i += 1
+                        else:
+                            break
+                    else:
+                        print ('error:lack of \"')
+                        exit()
+                    self.tokens.append(Token(5, temp))
+                    self.tokens.append(Token(4, '\"'))
+                i = self.skip_blank(i + 1)
+            elif content[i] in operators:
+                if (content[i] == '+' or content[i] == '-') and content[i + 1] == content[i]:
+                    self.tokens.append(Token(3, content[i] * 2))
+                    i = self.skip_blank(i + 2)
+                # if >= or <=
+                elif (content[i] == '>' or content[i] == '<') and content[i + 1] == '=':
+                    self.tokens.append(Token(3, content[i] + '='))
+                    i = self.skip_blank(i + 2)
+                # else
+                else:
+                    self.tokens.append(Token(3, content[i]))
+                    i = self.skip_blank(i + 1)
+
+        for token in self.tokens:
+          print ('(%s, %s)' % (token.type, token.value))
+
+
 def lexer():
     lexer = Lexer()
     lexer.main()
     for token in lexer.tokens:
         print ('(%s, %s)' % (token.type, token.value))
 
+
+if __name__ == '__main__':
+    try:
+        opts, argvs = getopt.getopt(sys.argv[1:], 's:lpah', ['help'])
+    except:
+        print (__doc__)
+        exit()
+
+    for opt, argv in opts:
+        if opt in ['-h', '--h', '--help']:
+            print (__doc__)
+            exit()
+        elif opt == '-s':
+            file_name = argv.split('.')[0]
+            source_file = open(argv, 'r')
+            content = source_file.read()
+        elif opt == '-l':
+            lexer()
 
